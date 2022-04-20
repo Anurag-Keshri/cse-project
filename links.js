7150bc0fa87733295c170629f71e32448dcff4d1
Anurag Keshri <anuragkeshri47@gmail.com>
2022-04-20 12:54:27 +0530

    fixed all the links

---
+++ links.js
@@ -0,0 +1,11 @@
+const facebook = "https://www.facebook.com/people/Raj-Agrawal/100011222381046/";
+const twitter = "https://twitter.com/SinuSecu";
+const instagram = "https://www.instagram.com/_samriddhi_g_/";
+const linkedin = "https://www.linkedin.com/in/samriddhi-gupta-939a91221/";
+
+document.querySelectorAll('.urlFacebook').forEach((element) => {element.href = facebook;});
+document.querySelectorAll('.urlTwitter').forEach((element) => {element.href = twitter;});
+document.querySelectorAll('.urlInstagram').forEach((element) => {element.href = instagram;});
+document.querySelectorAll('.urlLinkedin') .forEach((element) => {element.href = linkedin;});
+
+
